e7aceabe137db2b2bbb8a2841b9bcdc0c0f73195
eastriverlee <dan0720@naver.com>
2021-09-15 07:56:19 +0900

    Initial Commit

---
+++ learner/learnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnerApp.swift
+//  learner
+//
+//  Created by dan lee on 2021/09/15.
+//
+
+import SwiftUI
+
+@main
+struct learnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
